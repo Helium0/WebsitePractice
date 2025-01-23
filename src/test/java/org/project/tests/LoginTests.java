b02f1ef624e273d5d4dd9e1617b2cc7a3514437d
Patryk Prentki <patryk.prentki90@gmail.com>
2025-01-23 23:57:43 +0100

    Added Assertions and class fields

--- src/test/java/org/project/tests/LoginTests.java
+++ src/test/java/org/project/tests/LoginTests.java
@@ -1,20 +1,32 @@
 package org.project.tests;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.project.BasePage;
 import org.project.helper.DataProv;
 import org.project.pages.LoginPage;
+import org.project.utilities.ReadProperties;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.io.IOException;
+
 
 public class LoginTests extends BasePage {
 
 
+    private ReadProperties readProperties = new ReadProperties(); // class for reading private credentials from separated file
+
+    // Error texts from login page. Used for assertions.
+    private final String EMAIL_REQUIRED = "An email address required.";
+    private final String PASSWORD_REQUIRED = "Password is required.";
+    private final String INVALID_DATA = "Authentication failed.";
 
     /* I used provider class for learning purpose. With this class I can read the data from JSON file and pass
     throught it. This allow me to reduce another tests however I leave it like that.
      */
 
-    @Test(dataProvider = "dataProvider", dataProviderClass = DataProv.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProv.class,dependsOnMethods = "loginToAccountWithoutEmail")
     public void loginsWithDataProvider(String data) {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickSignInButtonAtTheBeginning();
@@ -22,38 +34,53 @@ public class LoginTests extends BasePage {
         loginPage.provideEmail(users[0]);
         loginPage.providePassword(users[1]);
         loginPage.clickSubmitLoginButton();
+
     }
 
     @Test
-    public void loginToAccountWithInvalidData() {
+    public void loginToAccountWithInvalidData() throws IOException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickSignInButtonAtTheBeginning();
-        loginPage.provideEmail("patryk.tester666@gmail.com");
-        loginPage.providePassword("666tester");
+        loginPage.provideEmail(readProperties.readValue("wrongEmail"));
+        loginPage.providePassword(readProperties.readValue("wrongPassword"));
         loginPage.clickSubmitLoginButton();
+        WebElement element = driver.findElement(By.xpath("//li[text()='Authentication failed.']"));
+
+        Assert.assertEquals(element.getText(),INVALID_DATA);
+
     }
 
-    @Test
+    @Test(dependsOnMethods = "loginToAccountWithInvalidData")
     public void loginToAccountWithoutData() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickSignInButtonAtTheBeginning();
         loginPage.clickSubmitLoginButton();
+        WebElement element = driver.findElement(By.xpath("//li[text()='An email address required.']"));
+
+        Assert.assertEquals(element.getText(),EMAIL_REQUIRED);
+
     }
 
-    @Test
-    public void loginToAccountWithoutPassword() {
+    @Test(dependsOnMethods = "loginToAccountWithoutData")
+    public void loginToAccountWithoutPassword() throws IOException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickSignInButtonAtTheBeginning();
-        loginPage.provideEmail("patryk.tester666@gmail.com");
+        loginPage.provideEmail(readProperties.readValue("wrongEmail"));
         loginPage.clickSubmitLoginButton();
+        WebElement element = driver.findElement(By.xpath("//li[text()='Password is required.']"));
+
+        Assert.assertEquals(element.getText(),PASSWORD_REQUIRED);
     }
 
-    @Test
-    public void loginToAccountWithoutEmail() {
+    @Test(dependsOnMethods = "loginToAccountWithoutPassword")
+    public void loginToAccountWithoutEmail() throws IOException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickSignInButtonAtTheBeginning();
-        loginPage.providePassword("666tester");
+        loginPage.providePassword(readProperties.readValue("wrongPassword"));
         loginPage.clickSubmitLoginButton();
+        WebElement element = driver.findElement(By.xpath("//li[text()='An email address required.']"));
+
+        Assert.assertEquals(element.getText(),EMAIL_REQUIRED);
     }
 
 //    @Test
