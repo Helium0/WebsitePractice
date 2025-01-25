5bcb437065d6c03e741b27f809265bb6821791dd
Patryk Prentki <patryk.prentki90@gmail.com>
2025-01-25 23:32:21 +0100

    Customer class for generate dynamic customers

---
+++ src/test/java/org/project/helper/Customer.java
@@ -0,0 +1,43 @@
+package org.project.helper;
+
+public class Customer {
+
+    String customerName;
+    String customerLastName;
+    String customerEmail;
+    String customerPassword;
+
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
+
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
+
+}
