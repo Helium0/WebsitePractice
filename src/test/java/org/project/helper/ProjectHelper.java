f68c717dbe3d8c2e3e5722de44e36f0ab557a196
Patryk Prentki <patryk.prentki90@gmail.com>
2025-01-27 23:58:17 +0100

    Added another method for soft assertions

--- src/test/java/org/project/helper/ProjectHelper.java
+++ src/test/java/org/project/helper/ProjectHelper.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.project.BasePage;
+import org.testng.asserts.SoftAssert;
+
 import java.time.Duration;
 
 
@@ -22,6 +24,9 @@ public class ProjectHelper extends BasePage {
 
     public void waitForElement(WebElement element) {
         webDriverWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+    public SoftAssert softAssert() {
+        return new SoftAssert();
     }
 
 
