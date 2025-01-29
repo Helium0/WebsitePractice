fae70ef7235b722a4a23d6c3488cb37b58b4fba0
Patryk Prentki <patryk.prentki90@gmail.com>
2025-01-29 23:54:51 +0100

    ProductComponent class for nested elements and get particular value

---
+++ src/test/java/org/project/helper/ProductComponent.java
@@ -0,0 +1,29 @@
+package org.project.helper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ProductComponent extends BaseComponent {
+
+
+    private final By PRODUCT = By.xpath("//a[@class='product-name']");
+    private final By PRODUCT_PRICE = By.xpath("//span[@class='price product-price']");
+
+
+    public ProductComponent(WebElement root) {
+        super(root);
+    }
+
+    public String getProductName() {
+        return root.findElement(PRODUCT).getText();
+    }
+
+    public String getProductPrice() {
+        return root.findElement(PRODUCT_PRICE).getText();
+    }
+
+
+
+
+
+}
