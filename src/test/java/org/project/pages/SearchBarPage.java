649d9403a0a0a1ca439f6c40ec4c97cb911d2b58
Patryk Prentki <patryk.prentki90@gmail.com>
2025-01-30 17:18:51 +0100

    SearchBar page with methods and locators

---
+++ src/test/java/org/project/pages/SearchBarPage.java
@@ -0,0 +1,53 @@
+package org.project.pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.project.BasePage;
+import org.project.helper.ProductComponent;
+import java.util.List;
+import java.util.function.Predicate;
+
+
+
+public class SearchBarPage extends BasePage {
+
+
+    @FindBy(id = "search_query_top")
+    private WebElement searchBarInput;
+
+    @FindBy(name = "submit_search")
+    private WebElement searchBarSubmit;
+
+    @FindBy(xpath = "//div[@class='product-container']")
+    private List<WebElement> productsList;
+
+    public SearchBarPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setSearchBarInput(String product) {
+        searchBarInput.sendKeys(product);
+    }
+    public void searchBarClick() {
+        searchBarSubmit.click();
+    }
+
+    public List<ProductComponent> getAllDisplayedProducts() {
+        return productsList.stream().map(a-> new ProductComponent(a))
+                .toList();
+    }
+
+    public ProductComponent getDisplayedProduct(Predicate<ProductComponent> condition) {
+        return getAllDisplayedProducts().stream()
+                .filter(condition).findFirst().orElseThrow();
+    }
+
+
+
+
+
+
+}
