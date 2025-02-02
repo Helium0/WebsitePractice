9452519170331622100308bca646213f76ffc445
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-02 19:20:39 +0100

    Created new Page for define customer address

---
+++ src/test/java/org/project/pages/DefineCustomerAdressPage.java
@@ -0,0 +1,76 @@
+package org.project.pages;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.project.helper.Customer;
+
+public class DefineCustomerAdressPage {
+
+    @FindBy(id = "address1")
+    private WebElement customerAddressInput;
+    @FindBy(id = "city")
+    private WebElement customerCityInput;
+    @FindBy(id = "postcode")
+    private WebElement customerPostalCode;
+    @FindBy(id = "phone_mobile")
+    private WebElement customerMobilePhone;
+    @FindBy(name = "alias")
+    private WebElement assignCustomerAddressInput;
+    @FindBy(xpath = "//span[text()='Add my first address']")
+    private WebElement customerFirstAdressButton;
+    @FindBy(name = "alias")
+    private WebElement customerAssignAddress;
+    @FindBy(id = "submitAddress")
+    private WebElement saveAddressButton;
+    @FindBy(id = "id_state")
+    private Select selectCustomerState;
+
+    private Faker faker;
+    private Customer customer;
+
+
+    public DefineCustomerAdressPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+    public void clickOnAddCustomerFirstAddress() {
+        customerFirstAdressButton.click();
+    }
+    public void assignCustomerAddressInput(String address) {
+        customerAssignAddress.clear();
+        customerAssignAddress.sendKeys(address);
+    }
+    public void clickOnSaveAddressButton() {
+        saveAddressButton.click();
+    }
+
+    public void fillCustomerAddress() {
+         customerAddressInput.sendKeys(setCustomerAddress().getCustomerAddress());
+         customerCityInput.sendKeys(setCustomerAddress().getCustomerCity());
+         customerPostalCode.sendKeys(setCustomerAddress().getCustomerPostalCode());
+         customerMobilePhone.sendKeys(setCustomerAddress().getCustomerMobilePhone());
+    }
+
+    private Customer setCustomerAddress() {
+        customer = new Customer();
+        faker = new Faker();
+        customer.setCustomerAddress(faker.address().streetAddress());
+        customer.setCustomerCity(faker.address().cityName());
+        customer.setCustomerPostalCode(customerZipCode());
+        customer.setCustomerMobilePhone(faker.phoneNumber().cellPhone());
+        return customer;
+    }
+    private String customerZipCode() {
+        faker = new Faker();
+        String zip = faker.address().zipCode();
+        if(faker.address().zipCode().length() >= 5) {
+             zip = faker.address().zipCode().substring(0,5);
+        }
+        return  zip;
+    }
+
+
+}
