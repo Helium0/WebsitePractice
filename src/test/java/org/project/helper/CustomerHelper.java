2145d9855978fe9342ed5e35840639a04ca24a2c
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-04 23:56:12 +0100

    Created class with necessary methods to create customer object by builder

---
+++ src/test/java/org/project/helper/CustomerHelper.java
@@ -0,0 +1,36 @@
+package org.project.helper;
+
+import com.github.javafaker.Faker;
+
+public class CustomerHelper {
+
+    private Faker fakerMethod() {
+        return new Faker();
+    }
+
+    private String date() {
+        return fakerMethod().date().birthday(18,90).toString();
+    }
+
+    private String customerZipCode() {
+        String zip = fakerMethod().address().zipCode();
+        if(fakerMethod().address().zipCode().length() >= 5) {
+            zip = fakerMethod().address().zipCode().substring(0,5);
+        }
+        return  zip;
+    }
+
+    public Customer.CustomerBuilder customerObject() {
+        Customer.CustomerBuilder customerBuilder = new Customer.CustomerBuilder()
+                .setCustomerName(fakerMethod().name().firstName());
+        customerBuilder.setCustomerName(fakerMethod().name().lastName());
+        customerBuilder.setCustomerEmail(fakerMethod().internet().emailAddress());
+        customerBuilder.setCustomerPassword(fakerMethod().internet().password());
+        customerBuilder.setCustomerBirthday(date().split(" "));
+        customerBuilder.setCustomerAddress(fakerMethod().address().streetAddress());
+        customerBuilder.setCustomerCity(fakerMethod().address().cityName());
+        customerBuilder.setCustomerPostalCode(customerZipCode());
+        customerBuilder.setCustomerMobilePhone(fakerMethod().phoneNumber().cellPhone());
+        return customerBuilder;
+    }
+}
