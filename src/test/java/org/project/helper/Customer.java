5592e4834e351a42be2ff6c202247cbbd89b2320
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-04 23:55:03 +0100

    Implemented Builder creational pattern because was so many class fields and it`s good practice and helps with bigger objects

--- src/test/java/org/project/helper/Customer.java
+++ src/test/java/org/project/helper/Customer.java
@@ -1,6 +1,5 @@
 package org.project.helper;
 
-import com.github.javafaker.Faker;
 
 public class Customer {
 
@@ -16,78 +15,119 @@ public class Customer {
 
 
 
-    public void setCustomerAddress(String customerAddress) {
-        this.customerAddress = customerAddress;
-    }
-    public String getCustomerAddress() {
-        return customerAddress;
-    }
-    public void setCustomerCity(String customerCity) {
-        this.customerCity = customerCity;
-    }
-    public String getCustomerCity() {
-        return customerCity;
-    }
-    public void setCustomerPostalCode(String customerPostalCode) {
-        this.customerPostalCode = customerPostalCode;
-    }
-    public String getCustomerPostalCode() {
-        return customerPostalCode;
-    }
-    public void setCustomerMobilePhone(String customerMobilePhone) {
-        this.customerMobilePhone = customerMobilePhone;
-    }
-    public String getCustomerMobilePhone() {
-        return customerMobilePhone;
+    private Customer(CustomerBuilder builder) {
+        this.customerName = builder.customerName;
+        this.customerLastName = builder.customerLastName;
+        this.customerEmail = builder.customerEmail;
+        this.customerPassword = builder.customerPassword;
+        this.customerBirthday = builder.customerBirthday;
+        this.customerAddress = builder.customerAddress;
+        this.customerCity = builder.customerCity;
+        this.customerPostalCode = builder.customerPostalCode;
+        this.customerMobilePhone = builder.customerMobilePhone;
     }
 
 
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
+    public static class CustomerBuilder {
+        private String customerName;
+        private String customerLastName;
+        private String customerEmail;
+        private String customerPassword;
+        private String[] customerBirthday;
+        private String customerAddress;
+        private String customerCity;
+        private String customerPostalCode;
+        private String customerMobilePhone;
 
-    public String getCustomerName() {
-        return customerName;
-    }
 
-    public void setCustomerLastName(String customerLastName) {
-        this.customerLastName = customerLastName;
-    }
-
-    public String getCustomerLastName() {
-        return customerLastName;
-    }
-
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
-
-    public String getCustomerEmail() {
-        return customerEmail;
-    }
-
-    public void setCustomerPassword(String customerPassword) {
-        this.customerPassword = customerPassword;
-    }
-
-    public String getCustomerPassword() {
-        return customerPassword;
-    }
-    public void setCustomerBirthday(String[] customerBirthday) {
-        this.customerBirthday = customerBirthday;
-    }
-    public String[] getCustomerBirthday() {
-        if(customerBirthday[2].charAt(0)=='0'){
-            customerBirthday[2] = customerBirthday[2].substring(1);
+        public CustomerBuilder setCustomerName(String customerName) {
+            this.customerName = customerName;
+            return this;
         }
-        return customerBirthday;
-    }
-    private String date() {
-        return new Faker().date().birthday(18,90).toString();
-    }
 
-    public String [] tab() {
-        return date().split(" ");
+        public String getCustomerName() {
+            return customerName;
+        }
+
+        public CustomerBuilder setCustomerLastName(String customerLastName) {
+            this.customerLastName = customerLastName;
+            return this;
+        }
+
+        public String getCustomerLastName() {
+            return customerLastName;
+        }
+
+        public CustomerBuilder setCustomerEmail(String customerEmail) {
+            this.customerEmail = customerEmail;
+            return this;
+        }
+
+        public String getCustomerEmail() {
+            return customerEmail;
+        }
+
+        public CustomerBuilder setCustomerPassword(String customerPassword) {
+            this.customerPassword = customerPassword;
+            return this;
+        }
+
+        public String getCustomerPassword() {
+            return customerPassword;
+        }
+
+        public CustomerBuilder setCustomerBirthday(String[] customerBirthday) {
+            this.customerBirthday = customerBirthday;
+            return this;
+        }
+
+        public String[] getCustomerBirthday() {
+            if(customerBirthday[2].charAt(0)=='0'){
+                customerBirthday[2] = customerBirthday[2].substring(1);
+            }
+            return customerBirthday;
+        }
+
+        public CustomerBuilder setCustomerAddress(String customerAddress) {
+            this.customerAddress = customerAddress;
+            return this;
+        }
+
+        public String getCustomerAddress() {
+            return customerAddress;
+        }
+
+        public CustomerBuilder setCustomerCity(String customerCity) {
+            this.customerCity = customerCity;
+            return this;
+        }
+
+        public String getCustomerCity() {
+            return customerCity;
+        }
+
+        public CustomerBuilder setCustomerPostalCode(String customerPostalCode) {
+            this.customerPostalCode = customerPostalCode;
+            return this;
+        }
+
+        public String getCustomerPostalCode() {
+            return customerPostalCode;
+        }
+
+        public CustomerBuilder setCustomerMobilePhone(String customerMobilePhone) {
+            this.customerMobilePhone = customerMobilePhone;
+            return this;
+        }
+
+        public String getCustomerMobilePhone() {
+            return customerMobilePhone;
+        }
+
+        public Customer build() {
+            return new Customer(this);
+        }
+
     }
 
 }
