59b8f13445a390bcf892819fd5b09217a2ff143a
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-05 23:50:23 +0100

    Finished tests for DefineCustomerAdressTests class

--- src/test/java/org/project/tests/DefineCustomerAdressTests.java
+++ src/test/java/org/project/tests/DefineCustomerAdressTests.java
@@ -3,8 +3,10 @@ package org.project.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.project.BasePage;
+import org.project.helper.CustomerHelper;
 import org.project.helper.ProjectHelper;
 import org.project.pages.DefineCustomerAdressPage;
+import org.project.pages.DefineCustomerDetailsPage;
 import org.project.pages.LoginPage;
 import org.project.pages.NavigationBarPage;
 import org.project.utilities.ReadProperties;
@@ -20,38 +22,49 @@ public class DefineCustomerAdressTests extends BasePage {
     private ProjectHelper projectHelper;
     private ReadProperties readProperties;
     private NavigationBarPage navigationBarPage;
+    private CustomerHelper customerHelper;
+    private DefineCustomerDetailsPage defineCustomerDetailsPage;
 
     private final String CUSTOMER_FUTURE_REFERENCE_ADDRESS = "HONOLULU";
     private final String NO_CUSTOMER_ADDRESSES = "No addresses are available. Add a new address";
     private final String CUSTOMER_STATE = "Kansas";  // can be change for any US states
-
+    private final String CITY_REQUIRED = "111city is required.";
+    private final String POSTAL_CODE_REQUIRED = "11The Zip/Postal code you've entered is invalid. It must follow this format: 00000";
+    private final String PHONE_NUMBER_REQUIRED = "000You must register at least one phone number.";
+    private final String ADDRESS_REQUIRED = "address1 is required.";
+    private final String STATE_REQUIRED = "This country requires you to chose a State.";
 
 
     @Test
-    public void fillCustomerFirstAddress() throws IOException, InterruptedException {
+    public void fillCustomerFirstAddress() throws IOException {
         loginPage = new LoginPage(driver);
         defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
         projectHelper = new ProjectHelper();
         readProperties = new ReadProperties();
+        customerHelper = new CustomerHelper();
         navigationBarPage = new NavigationBarPage(driver);
         loginPage.clickSignInButtonAtTheBeginning();
         loginPage.provideEmail(readProperties.readValue("validEmail"));
         loginPage.providePassword(readProperties.readValue("validPassword"));
         loginPage.clickSubmitLoginButton();
         defineCustomerAdressPage.clickOnAddCustomerFirstAddress();
-        defineCustomerAdressPage.fillCustomerAddress();
+        defineCustomerAdressPage.fillCustomerFullAddress(customerHelper.customerObject());
         defineCustomerAdressPage.assignCustomerAddressInput(CUSTOMER_FUTURE_REFERENCE_ADDRESS);
-        projectHelper.select(defineCustomerAdressPage.customerState()).getOptions().stream().filter(a->a.getText().equals(CUSTOMER_STATE)).forEach(a->a.click());
+        projectHelper.select(defineCustomerAdressPage.customerState()).getOptions().stream().filter(a -> a.getText().equals(CUSTOMER_STATE)).forEach(a -> a.click());
         defineCustomerAdressPage.clickOnSaveCustomerAddressButton();
 
         int addressNumbers = driver.findElements(By.xpath("//div[@class='bloc_adresses row']//div")).size();
-        Assert.assertEquals(addressNumbers, 1);
+        if (addressNumbers >= 1) {
+            Assert.assertEquals(addressNumbers, 1);
+        } else {
+            Assert.fail("*** Assertion failed ***");
+        }
         navigationBarPage.signOutFromNavigationBar();
 
     }
 
     @Test(dependsOnMethods = "fillCustomerFirstAddress")
-    public void deleteFilledCustomerAddress() throws IOException, InterruptedException {
+    public void deleteFilledCustomerAddress() throws IOException {
         loginPage = new LoginPage(driver);
         defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
         navigationBarPage = new NavigationBarPage(driver);
@@ -65,9 +78,170 @@ public class DefineCustomerAdressTests extends BasePage {
         driver.switchTo().alert().accept();
 
         WebElement el = driver.findElement(By.xpath("//p[@class='alert alert-warning']"));
-        Assert.assertEquals(el.getText().trim(), NO_CUSTOMER_ADDRESSES);
+        if (el.getText().contains("No addresses")) {
+            Assert.assertEquals(el.getText().trim(), NO_CUSTOMER_ADDRESSES);
+        } else {
+            Assert.fail("*** Assertion failed ***");
+        }
         navigationBarPage.signOutFromNavigationBar();
 
     }
 
+    @Test(priority = 1)
+    public void fillCustomerFirstAddressWithoutCity() throws IOException {
+        loginPage = new LoginPage(driver);
+        defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
+        projectHelper = new ProjectHelper();
+        readProperties = new ReadProperties();
+        customerHelper = new CustomerHelper();
+        defineCustomerDetailsPage = new DefineCustomerDetailsPage(driver);
+        navigationBarPage = new NavigationBarPage(driver);
+        loginPage.clickSignInButtonAtTheBeginning();
+        loginPage.provideEmail(readProperties.readValue("validEmail"));
+        loginPage.providePassword(readProperties.readValue("validPassword"));
+        loginPage.clickSubmitLoginButton();
+        defineCustomerAdressPage.clickOnAddCustomerFirstAddress();
+        defineCustomerAdressPage.fillCustomerAddressWithoutCity(customerHelper.customerObject());
+        defineCustomerAdressPage.assignCustomerAddressInput(CUSTOMER_FUTURE_REFERENCE_ADDRESS);
+        projectHelper.select(defineCustomerAdressPage.customerState()).getOptions().stream().filter(a -> a.getText().equals(CUSTOMER_STATE)).forEach(a -> a.click());
+        defineCustomerAdressPage.clickOnSaveCustomerAddressButton();
+
+        if (defineCustomerDetailsPage.webElementsError().get(0).contains("city")) {
+            Assert.assertEquals(defineCustomerDetailsPage.webElementsError().get(0), CITY_REQUIRED);
+        } else {
+            Assert.fail("*** Assertion failed ***");
+        }
+        navigationBarPage.signOutFromNavigationBar();
+
+    }
+
+    @Test(priority = 1)
+    public void fillCustomerFirstAddressWithoutPostalCode() throws IOException {
+        loginPage = new LoginPage(driver);
+        defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
+        projectHelper = new ProjectHelper();
+        readProperties = new ReadProperties();
+        customerHelper = new CustomerHelper();
+        defineCustomerDetailsPage = new DefineCustomerDetailsPage(driver);
+        navigationBarPage = new NavigationBarPage(driver);
+        loginPage.clickSignInButtonAtTheBeginning();
+        loginPage.provideEmail(readProperties.readValue("validEmail"));
+        loginPage.providePassword(readProperties.readValue("validPassword"));
+        loginPage.clickSubmitLoginButton();
+        defineCustomerAdressPage.clickOnAddCustomerFirstAddress();
+        defineCustomerAdressPage.fillCustomerAddressWithoutPostalCode(customerHelper.customerObject());
+        defineCustomerAdressPage.assignCustomerAddressInput(CUSTOMER_FUTURE_REFERENCE_ADDRESS);
+        projectHelper.select(defineCustomerAdressPage.customerState()).getOptions().stream().filter(a -> a.getText().equals(CUSTOMER_STATE)).forEach(a -> a.click());
+        defineCustomerAdressPage.clickOnSaveCustomerAddressButton();
+
+        if (defineCustomerDetailsPage.webElementsError().get(0).contains("The Zip/Postal")) {
+            Assert.assertEquals(defineCustomerDetailsPage.webElementsError().get(0), POSTAL_CODE_REQUIRED);
+        } else {
+            Assert.fail("*** Assertion failed ***");
+        }
+        navigationBarPage.signOutFromNavigationBar();
+
+    }
+
+    @Test(priority = 1)
+    public void fillCustomerFirstAddressWithoutMobilePhone() throws IOException {
+        loginPage = new LoginPage(driver);
+        defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
+        projectHelper = new ProjectHelper();
+        readProperties = new ReadProperties();
+        customerHelper = new CustomerHelper();
+        defineCustomerDetailsPage = new DefineCustomerDetailsPage(driver);
+        navigationBarPage = new NavigationBarPage(driver);
+        loginPage.clickSignInButtonAtTheBeginning();
+        loginPage.provideEmail(readProperties.readValue("validEmail"));
+        loginPage.providePassword(readProperties.readValue("validPassword"));
+        loginPage.clickSubmitLoginButton();
+        defineCustomerAdressPage.clickOnAddCustomerFirstAddress();
+        defineCustomerAdressPage.fillCustomerAddressWithoutMobilePhone(customerHelper.customerObject());
+        defineCustomerAdressPage.assignCustomerAddressInput(CUSTOMER_FUTURE_REFERENCE_ADDRESS);
+        projectHelper.select(defineCustomerAdressPage.customerState()).getOptions().stream().filter(a -> a.getText().equals(CUSTOMER_STATE)).forEach(a -> a.click());
+        defineCustomerAdressPage.clickOnSaveCustomerAddressButton();
+
+        if (defineCustomerDetailsPage.webElementsError().get(0).contains("You must register")) {
+            Assert.assertEquals(defineCustomerDetailsPage.webElementsError().get(0), PHONE_NUMBER_REQUIRED);
+        } else {
+            Assert.fail("*** Assertion failed ***");
+        }
+        navigationBarPage.signOutFromNavigationBar();
+    }
+
+    @Test(priority = 1)
+    public void fillCustomerFirstAddressWithoutCountryState() throws IOException {
+        loginPage = new LoginPage(driver);
+        defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
+        projectHelper = new ProjectHelper();
+        readProperties = new ReadProperties();
+        customerHelper = new CustomerHelper();
+        defineCustomerDetailsPage = new DefineCustomerDetailsPage(driver);
+        navigationBarPage = new NavigationBarPage(driver);
+        loginPage.clickSignInButtonAtTheBeginning();
+        loginPage.provideEmail(readProperties.readValue("validEmail"));
+        loginPage.providePassword(readProperties.readValue("validPassword"));
+        loginPage.clickSubmitLoginButton();
+        defineCustomerAdressPage.clickOnAddCustomerFirstAddress();
+        defineCustomerAdressPage.fillCustomerFullAddress(customerHelper.customerObject());
+        defineCustomerAdressPage.assignCustomerAddressInput(CUSTOMER_FUTURE_REFERENCE_ADDRESS);
+        defineCustomerAdressPage.clickOnSaveCustomerAddressButton();
+
+        if (defineCustomerDetailsPage.webElementsError().get(0).contains("chose a State")) {
+            Assert.assertEquals(defineCustomerDetailsPage.webElementsError().get(0), STATE_REQUIRED);
+        } else {
+            Assert.fail("*** Assertion failed ***");
+        }
+        navigationBarPage.signOutFromNavigationBar();
+    }
+
+    @Test(priority = 1)
+    public void fillCustomerFirstAddressWithoutAddress() throws IOException {
+        loginPage = new LoginPage(driver);
+        defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
+        projectHelper = new ProjectHelper();
+        readProperties = new ReadProperties();
+        customerHelper = new CustomerHelper();
+        defineCustomerDetailsPage = new DefineCustomerDetailsPage(driver);
+        navigationBarPage = new NavigationBarPage(driver);
+        loginPage.clickSignInButtonAtTheBeginning();
+        loginPage.provideEmail(readProperties.readValue("validEmail"));
+        loginPage.providePassword(readProperties.readValue("validPassword"));
+        loginPage.clickSubmitLoginButton();
+        defineCustomerAdressPage.clickOnAddCustomerFirstAddress();
+        defineCustomerAdressPage.fillCustomerWithoutAddress(customerHelper.customerObject());
+        defineCustomerAdressPage.assignCustomerAddressInput(CUSTOMER_FUTURE_REFERENCE_ADDRESS);
+        defineCustomerAdressPage.clickOnSaveCustomerAddressButton();
+
+        if (defineCustomerDetailsPage.webElementsError().get(0).contains("address")) {
+            Assert.assertEquals(defineCustomerDetailsPage.webElementsError().get(0), ADDRESS_REQUIRED);
+        } else {
+            Assert.fail("*** Assertion failed ***");
+        }
+        navigationBarPage.signOutFromNavigationBar();
+    }
+
+    @Test(priority = 1)
+    public void fillCustomerFirstAddressWithoutRequiredFields() throws IOException {
+        loginPage = new LoginPage(driver);
+        defineCustomerAdressPage = new DefineCustomerAdressPage(driver);
+        projectHelper = new ProjectHelper();
+        readProperties = new ReadProperties();
+        customerHelper = new CustomerHelper();
+        defineCustomerDetailsPage = new DefineCustomerDetailsPage(driver);
+        navigationBarPage = new NavigationBarPage(driver);
+        loginPage.clickSignInButtonAtTheBeginning();
+        loginPage.provideEmail(readProperties.readValue("validEmail"));
+        loginPage.providePassword(readProperties.readValue("validPassword"));
+        loginPage.clickSubmitLoginButton();
+        defineCustomerAdressPage.clickOnAddCustomerFirstAddress();
+        defineCustomerAdressPage.assignCustomerAddressInput(CUSTOMER_FUTURE_REFERENCE_ADDRESS);
+        defineCustomerAdressPage.clickOnSaveCustomerAddressButton();
+
+        Assert.assertEquals(defineCustomerDetailsPage.webElementsError().size(), 5);
+
+        navigationBarPage.signOutFromNavigationBar();
+
+    }
 }
