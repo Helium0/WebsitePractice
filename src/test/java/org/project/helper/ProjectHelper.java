018faf3fcfbf77d740ee4fe88b17314b69907e76
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-05 23:53:08 +0100

    Rebuiled class slightly

--- src/test/java/org/project/helper/ProjectHelper.java
+++ src/test/java/org/project/helper/ProjectHelper.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.project.BasePage;
 import org.testng.asserts.SoftAssert;
 import java.time.Duration;
+import java.util.List;
+
 
 
 public class ProjectHelper extends BasePage {
@@ -16,15 +18,21 @@ public class ProjectHelper extends BasePage {
     private WebDriverWait webDriverWait() {
         return new WebDriverWait(driver,Duration.ofSeconds(10));
     }
+
     public void waitForElement(WebElement element) {
-        webDriverWait().until(ExpectedConditions.elementToBeClickable(element));
+        webDriverWait().until(ExpectedConditions.invisibilityOf(element));
     }
+
     public SoftAssert softAssert() {
         return new SoftAssert();
     }
+
     public Select select(WebElement element) {
         return new Select(element);
     }
 
+    public List<WebElement> streamMethod(WebElement element) {
+        return select(element).getOptions();
+    }
 
 }
