40bb2b798f2d006825ad4d97aab9faf41df230ef
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-09 18:00:32 +0100

    fixed tests

--- src/test/java/org/project/tests/SearchBarTests.java
+++ src/test/java/org/project/tests/SearchBarTests.java
@@ -1,7 +1,9 @@
 package org.project.tests;
 
+import org.openqa.selenium.Keys;
 import org.project.BasePage;
 import org.project.helper.ProductComponent;
+import org.project.helper.ProjectHelper;
 import org.project.pages.SearchBarPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,6 +13,7 @@ public class SearchBarTests extends BasePage {
 
 
     private SearchBarPage searchBarPage;
+    private ProjectHelper projectHelper;
     private final String FADED_DRESS = "Faded Short Sleeve T-shirts";
     private final String PRINTED_DRESS = "Printed Summer Dress";
 
@@ -25,8 +28,9 @@ public class SearchBarTests extends BasePage {
     @Test(groups = "functional")
     public void searchBarExactMatch() {
         searchBarPage = new SearchBarPage(driver);
-        searchBarPage.setSearchBarInput();
-        searchBarPage.searchBarClick();
+        projectHelper = new ProjectHelper();
+        projectHelper.actions(driver).click(searchBarPage.setSearchBarInput())
+                .sendKeys(FADED_DRESS).click(searchBarPage.searchBarClick()).perform();
         ProductComponent foundedDress = searchBarPage.getDisplayedProduct(a -> a.getProductName().equals(FADED_DRESS));
 
         Assert.assertEquals(foundedDress.getProductName(), FADED_DRESS);
@@ -34,11 +38,17 @@ public class SearchBarTests extends BasePage {
 
     }
 
-    @Test(groups = "functional")
+    @Test(groups = "functional", priority = 1)
     public void searchBarPartialMatch() {
         searchBarPage = new SearchBarPage(driver);
-        searchBarPage.setSearchBarInput();
-        searchBarPage.searchBarClick();
+        projectHelper = new ProjectHelper();
+        projectHelper.actions(driver).click(searchBarPage.setSearchBarInput())
+                .keyDown(Keys.CONTROL).sendKeys("A")
+                .keyUp(Keys.CONTROL)
+                .keyDown(Keys.BACK_SPACE)
+                .keyUp(Keys.BACK_SPACE)
+                .sendKeys(PRINTED_DRESS).click(searchBarPage.searchBarClick()).build().perform();
+
         ProductComponent dress = searchBarPage.getDisplayedProduct(a -> a.getProductName().equals(PRINTED_DRESS));
 
         Assert.assertEquals(dress.getProductName(), PRINTED_DRESS);
@@ -46,11 +56,16 @@ public class SearchBarTests extends BasePage {
 
     }
 
-    @Test(groups = "functional")
+    @Test(groups = "functional", priority = 2)
     public void invalidSearchNoResults() {
         searchBarPage = new SearchBarPage(driver);
-        searchBarPage.setSearchBarInput();
-        searchBarPage.searchBarClick();
+        projectHelper = new ProjectHelper();
+        projectHelper.actions(driver).click(searchBarPage.setSearchBarInput())
+                .keyDown(Keys.CONTROL).sendKeys("A")
+                .keyUp(Keys.CONTROL)
+                .keyDown(Keys.BACK_SPACE)
+                .keyUp(Keys.BACK_SPACE)
+                .sendKeys("boots").click(searchBarPage.searchBarClick()).perform();
         List<ProductComponent> boots = searchBarPage.getAllDisplayedProducts();
 
         Assert.assertEquals(boots.size(),0);
