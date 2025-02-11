60cb7820f8d070a1469b40836d7d6655a9d5c2a9
Patryk Prentki <patryk.prentki90@gmail.com>
2025-02-12 00:30:32 +0100

    Disable chrome pop-ups, autofill saves, enable automation message. Created browserMap method

--- src/test/java/org/project/BasePage.java
+++ src/test/java/org/project/BasePage.java
@@ -1,7 +1,5 @@
 package org.project;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
 import java.time.Duration;
--- src/test/java/org/project/WebDriverFactory.java
+++ src/test/java/org/project/WebDriverFactory.java
@@ -6,16 +6,20 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class WebDriverFactory {
 
     protected static WebDriver driver;
 
     public  WebDriver getCorrectBrowser(String browser) {
 
-        switch (browser) {
-            case "Chrome": return new ChromeDriver();
-            case "Firefox": return new FirefoxDriver();
-            case "Edge": return new EdgeDriver();
+        switch (browser.toLowerCase()) {
+            case "chrome": return new ChromeDriver(chromeOptions());
+            case "firefox": return new FirefoxDriver();
+            case "edge": return new EdgeDriver();
 
             default:
                 throw new RuntimeException("Incorrect browser: "+browser);
@@ -24,7 +28,18 @@ public class WebDriverFactory {
 
     public ChromeOptions chromeOptions() {
         ChromeOptions options =new ChromeOptions();
-        options.addArguments("--disable-gpu");
+        Map savePopUp = browserMap();
+        savePopUp.put("credentials_enable_service", false);
+        savePopUp.put("profile.password_manager_enabled", false);
+        savePopUp.put("autofill.profile_enabled", false);
+        options.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking"));
+        options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
+        options.setExperimentalOption("prefs", savePopUp);
         return options;
     }
+
+    private Map browserMap() {
+        return new HashMap<>();
+    }
+
 }
